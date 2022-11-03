1c7b58a116b3a626ff6aed5bd3f9096644c1acd7
Matthew Tole <matthew@commonroom.io>
2022-11-03 14:29:41 -0700

    feat(2017): Complete 2017 day 5
    Got stuck on the same bug I always hit at some point: part 1 changing the input data and causing part 2 to have the correct value.
    
    Changed the way I loaded data in the index file and it works.

--- 2017/index.ts
+++ 2017/index.ts
@@ -3,25 +3,28 @@ import {execute, runWithPerf} from '../shared/utils';
 const days = [
   async () => {
     const {parse, part1, part2} = await import('./01');
-    const data = await parse();
     console.log(' - DAY 01 - ');
-    console.log('Part 1:', runWithPerf(part1, data));
-    console.log('Part 2:', runWithPerf(part2, data));
+    console.log('Part 1:', runWithPerf(part1, await parse()));
+    console.log('Part 2:', runWithPerf(part2, await parse()));
   },
   async () => {
     const {parse, part1, part2} = await import('./02');
-    const data = await parse();
     console.log(' - DAY 02 - ');
-    console.log('Part 1:', runWithPerf(part1, data));
-    console.log('Part 2:', runWithPerf(part2, data));
+    console.log('Part 1:', runWithPerf(part1, await parse()));
+    console.log('Part 2:', runWithPerf(part2, await parse()));
   },
   async () => {},
   async () => {
     const {parse, part1, part2} = await import('./04');
-    const data = await parse();
     console.log(' - DAY 04 - ');
-    console.log('Part 1:', runWithPerf(part1, data));
-    console.log('Part 2:', runWithPerf(part2, data));
+    console.log('Part 1:', runWithPerf(part1, await parse()));
+    console.log('Part 2:', runWithPerf(part2, await parse()));
+  },
+  async () => {
+    const {parse, part1, part2} = await import('./05');
+    console.log(' - DAY 05 - ');
+    console.log('Part 1:', runWithPerf(part1, await parse()));
+    console.log('Part 2:', runWithPerf(part2, await parse()));
   },
 ];
 
