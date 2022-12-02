7cf33a01987bcad9897a03614e4e3aa4c107b38d
Matthew Tole <matthewtole@gmail.com>
2022-12-01 20:10:16 -0800

    chore: Delete unused utils file

--- 2021/utils.ts
+++
@@ -1,5 +0,0 @@
-export const sum = (nums: Array<number>) =>
-  nums.reduce((total, num) => total + num, 0);
-
-export const product = (nums: Array<number>) =>
-  nums.reduce((total, num) => total * num, 1);
